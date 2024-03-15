00689af1e01557ad5ef232bd7e567fd482815524
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-15 09:00:45 +0200

    Add files via upload

---
+++ YMusic/core/userbot.py
@@ -0,0 +1,95 @@
+from YMusic import call
+# from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls import StreamType
+from pytgcalls.types import MediaStream
+# from pytgcalls.types import AudioParameters, VideoParameters, AudioQuality, VideoQuality
+
+audio_file = "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+
+
+async def playAudio(chat_id, audio_file=audio_file):
+    try:
+        await call.join_group_call(
+            chat_id,
+            MediaStream(
+                audio_file,
+                video_flags=MediaStream.IGNORE,
+            ),
+        )
+        return True, None
+    except Exception as e:
+        return False, f"Error:- <code>{e}</code>"
+
+
+async def playVideo(chat_id, video_file=audio_file):
+    try:
+        await call.join_group_call(
+            chat_id,
+            MediaStream(
+                video_file,
+            ),
+        )
+        return True, None
+    except Exception as e:
+        return False, f"Error:- <code>{e}</code>"
+
+
+async def pause(chat_id):
+    try:
+        await call.pause_stream(
+            chat_id,
+        )
+        return "-› تم الايـقاف مؤقـتا ."
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
+
+
+async def resume(chat_id):
+    try:
+        await call.resume_stream(
+            chat_id,
+        )
+        return "-› تم اكمـال التشـغيل ."
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
+
+
+async def mute(chat_id):
+    try:
+        await call.mute_stream(
+            chat_id,
+        )
+        return "-› تم الكـتم بنجـاح ."
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
+
+
+async def unmute(chat_id):
+    try:
+        await call.unmute_stream(
+            chat_id,
+        )
+        return "-› تم رفـع الكـتم ."
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
+
+
+async def changeVolume(chat_id, volume: int = 200):
+    try:
+        await call.change_volume_call(
+            chat_id,
+            volume,
+        )
+        return f"-› تم ضبط الصوت على :- {volume}%"
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
+
+
+async def stop(chat_id):
+    try:
+        await call.leave_group_call(
+            chat_id,
+        )
+        return "-› تم ايقـاف التشغـيل ."
+    except Exception as e:
+        return f"Error:- <code>{e}</code>"
