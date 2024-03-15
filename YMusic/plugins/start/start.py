75a2c148144c46caf9853b9c73e093779e242b2d
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-15 09:03:08 +0200

    Add files via upload

---
+++ YMusic/plugins/start/start.py
@@ -0,0 +1,20 @@
+from pyrogram import filters
+from YMusic import app
+import config
+from filters import command
+
+
+START_COMMAND = ["الاوامر", "ST"]
+
+@app.on_message(command(START_COMMAND)
+	)
+async def _start(_, message):
+	await message.reply_text("""-› اليك قائمة الأوامر من سورس تيتو
+
+-› تشغيل ~ تشغيل ملف صوتي في المجموعة
+-› تكرار ~ سيقوم تكرار الملف 100 مرة 
+-› تخطي ~ تخطي الملف الصوتي أونلاين 
+-› فيديو ~  تشغيل ملف فيديو في المجموعة
+-› فيد ~ تنزيل ملف فيديو بحث
+
+~ مطور السورس @WZAERE . """)
